70f4c453dcaa495e518384222d442265f05d93f3
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2022-03-17 16:49:47 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/HarbCareerDateTimeParser.java
@@ -0,0 +1,18 @@
+package ru.job4j.grabber.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class HarbCareerDateTimeParser implements DateTimeParser {
+
+    @Override
+    public LocalDateTime parse(String parse) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd'T'HH:mm:ssX");
+        return sdf.parse(parse)
+                .toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+}
