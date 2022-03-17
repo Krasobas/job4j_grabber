758dbcfefe39529c63c0cccd3439aa0b00600ec9
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2022-03-17 20:30:59 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.StringJoiner;
 
 public class HabrCareerParse {
 
@@ -14,10 +15,28 @@ public class HabrCareerParse {
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
-    public static void main(String[] args) throws IOException {
+    private String retrieveDescription(String link) throws IOException {
+        StringJoiner rsl = new StringJoiner(System.lineSeparator());
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".job_show_description__vacancy_description");
+        rows.forEach(row -> {
+            Element descriptionElement = row.select(".style-ugc").first();
+            descriptionElement.children().forEach(child -> {
+                if ("ul".equals(child.tagName())) {
+                    child.children().forEach(li -> rsl.add("\t– " + li.text()));
+                } else {
+                    rsl.add(child.text());
+                }
+            });
+        });
+        return rsl.toString();
+    }
+
+    public void init() throws IOException {
         StringBuilder url = new StringBuilder().append(PAGE_LINK).append("?page=");
         int start = PAGE_LINK.length() + 6;
-        for (int page = 1; page < 6; page++) {
+        for (int page = 1; page < 2; page++) {
             url.append(page);
             Connection connection = Jsoup.connect(url.toString());
             Document document = connection.get();
@@ -30,8 +49,22 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 System.out.printf("%s %s %s%n", time, vacancyName, link);
+                try {
+                    System.out.println(retrieveDescription(link));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("%n%s%n%n", "=".repeat(30));
             });
             url.delete(start, url.length());
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            new HabrCareerParse().init();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
