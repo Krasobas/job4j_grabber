98ba49610ee134d92e99bdcf7d716553c3d97f5e
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2022-03-17 17:41:10 +0300

    2.1.1. Парсинг - итерация по страницам

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -15,17 +15,24 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element timeElement = row.select(".vacancy-card__date").first();
-            String time = timeElement.child(0).attr("datetime");
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s %s%n", time, vacancyName, link);
-        });
+        StringBuilder url = new StringBuilder().append(PAGE_LINK).append("?page=");
+        int start = PAGE_LINK.length() + 6;
+        for (int page = 1; page < 6; page++) {
+            url.append(page);
+            Connection connection = Jsoup.connect(url.toString());
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element timeElement = row.select(".vacancy-card__date").first();
+                String time = timeElement.child(0).attr("datetime");
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.printf("%s %s %s%n", time, vacancyName, link);
+            });
+            url.delete(start, url.length());
+        }
+
     }
 }
