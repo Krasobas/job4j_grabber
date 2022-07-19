7623277bfd36f93413fa4d7249d927365ccb5111
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2022-07-19 19:08:32 +0200

    2. SQL Select by company [#2760]

---
+++ db/scripts/selectByCompany.sql
@@ -0,0 +1,13 @@
+select p.name, c.name from person as p
+join company as c
+on p.company_id = c.id
+where c.id != 5;
+
+select c.name, count(p.*) as persons from company as c
+join person as p
+on c.id = p.company_id
+group by c.name
+having count(p.*) >= all (select count(p.*) as persons from company as c
+                            join person as p
+                            on c.id = p.company_id
+                            group by c.name);
