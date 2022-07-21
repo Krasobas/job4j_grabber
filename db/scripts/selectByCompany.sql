346ce8afdc956b5597ccf741a2a0eb4efbca43b8
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2022-07-21 15:35:59 +0200

    2. SQL Select by company [#2760] Исправил второй запрос.

--- db/scripts/selectByCompany.sql
+++ db/scripts/selectByCompany.sql
@@ -3,11 +3,18 @@ join company as c
 on p.company_id = c.id
 where c.id != 5;
 
+-- select c.name, count(p.*) as persons from company as c
+-- join person as p
+-- on c.id = p.company_id
+-- group by c.name
+-- having count(p.*) >= all (select count(p.*) from person as p
+--                             group by p.company_id);
+
 select c.name, count(p.*) as persons from company as c
 join person as p
 on c.id = p.company_id
 group by c.name
-having count(p.*) >= all (select count(p.*) from person as p
-                            group by p.company_id)
-order by persons desc
-limit 1;
+having count(p.*) = (select count(p.*) from person as p
+                     group by p.company_id
+                     order by count(*) desc
+                     limit 1);
